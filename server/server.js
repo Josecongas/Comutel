d10f1fb8cdb93a893ee3a0fa68ee67d471d370dd
jgascona <webcongas@gmail.com>
2018-10-25 17:52:19 +0200

    Prueba de subida a Heroku - Frontend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 require('./config/config');
+const path = require('path');
 
 const mongoose = require('mongoose');
 const colors = require('colors');
@@ -15,6 +16,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// Habilitar la carpeta Public
+app.use(express.static(path.resolve(__dirname, '../comutel-app/src')));
 
 // CRUD de Vehiculo
 app.use(require('./rutas/vehiculo'));
